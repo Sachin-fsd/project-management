77bcbb6c3b2837867b7e05507d8d5ddd66c31014
Sachin <sachineducational555@gmail.com>
2024-12-19 15:33:30 +0530

    Updated: DATABASE_URL in env file

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,11 @@
+'use server'
 const mongoose = require('mongoose'); // CommonJS import
 
 export async function ConnectDb() {
-    const url = 'mongodb+srv://sachineducational:sachin@cluster0.mybvg88.mongodb.net/project_management?retryWrites=true&w=majority';
     
     try {
         // Make sure the Mongoose connection is established with await
-        await mongoose.connect(url);
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error('Database connection error:', err);
